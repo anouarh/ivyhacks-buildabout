768f63b6d6f50774a3a5e84f4e1f527fcd467366
Anouar <anouar.hilali1@gmail.com>
2020-10-01 20:12:01 +0100

    Initial commit

---
+++ Backend/backend-ba/src/main/java/com/buildabout/backendba/BackendBaApplication.java
@@ -0,0 +1,13 @@
+package com.buildabout.backendba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBaApplication.class, args);
+	}
+
+}
---
+++ Backend/backend-ba/src/test/java/com/buildabout/backendba/BackendBaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildabout.backendba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
