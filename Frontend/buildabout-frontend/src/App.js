dbf34236c324dc06b2b94b0363fe4c582951f24d
r2d2117 <woodfinemacpherson@gmail.com>
2020-10-02 16:21:40 -0400

    preparing frontend assets

--- Frontend/buildabout-frontend/src/App.js
+++ Frontend/buildabout-frontend/src/App.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import UserHomePage from './components/user-hompage'
 import './App.css';
+import toolboxlogo from './images/cartoontools@2x.png';
+
+
+
+function Welcome(props) {
+    return (<div id="parent">
+	    <h1> Hello, {props.name} </h1>
+	    <p> Hope everything is {props.state} </p>
+	    </div>
+	   );
+    
+}
+
+function WhatMaterials(){
+    return (<div className="MaterialPrompt">
+	    <div className="MaterialPromptLogo"><img src={toolboxlogo} alt="toolbox logo" />
+	    </div>
+	    <div className="MaterialPromptLine">
+	    </div>
+	    </div>)
+
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Blah blah
-        </p>
-      </header>
-      <UserHomePage></UserHomePage>
-    </div>
+    return (
+	    <div className="App">
+
+	<WhatMaterials> </WhatMaterials>
+	    </div>
   );
 }
 
