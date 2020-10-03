6eafa7c85519e6e944fa3adb662506ffc46d860f
Anouar <anouar.hilali1@gmail.com>
2020-10-03 23:23:27 +0100

    Fix projects page, add CORS policy to spring boot

--- Frontend/buildabout-frontend/src/pages/projects/projects-page.js
+++ Frontend/buildabout-frontend/src/pages/projects/projects-page.js
@@ -2,20 +2,35 @@ import React, { Component } from "react";
 import "./projects-page.css";
 
 class Projects extends Component {
+
+  state = {
+    materials: []
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:8080/materials/getAllMaterials')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ contacts: data })
+    })
+    .catch(console.log)
+  }
+
   render() {
     return (
       <div className="main">
         <div className="tools">
-        <h3>Add Materials</h3>
+          <h3>Add Materials</h3>
           <wired-search-input placeholder="Search Materials"></wired-search-input>
         </div>
         <div className="materials">
-        <h3>Add Tools</h3>
+          <h3>Add Tools</h3>
           <wired-search-input placeholder="Search Tools"></wired-search-input>
         </div>
         <div className="toolbox">
           <h3>Your ToolBox</h3>
-          <button>Generate</button>
+          {/* <div className="generate-button">Generate</div> */}
+          <wired-button elevation="2" >Generate</wired-button>
         </div>
       </div>
     );
