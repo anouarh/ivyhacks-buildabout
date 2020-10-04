4aea24619b151337915aa8dfdb7ea5cb24ee8fc4
Anouar <anouar.hilali1@gmail.com>
2020-10-04 11:22:38 +0100

    Fix home buttons for now

--- Frontend/buildabout-frontend/src/pages/home/home-page.js
+++ Frontend/buildabout-frontend/src/pages/home/home-page.js
@@ -11,34 +11,42 @@ class Home extends Component {
             <h4 className="faded">
               Use ML to generate projects based on existing materials and tools.
             </h4>
-            <a href="/projects">
-              <wired-button elevation="2">Go To Projects</wired-button>
-            </a>
+            <div className="button-container">
+              <a href="/projects">
+                <wired-button elevation="2">Go To Projects</wired-button>
+              </a>
+            </div>
           </div>
 
           <div className="activity-card red">
             <h1>Watch Experiments</h1>
             <h4 className="faded">Watch all kinds of science experiments.</h4>
-            <a href="/watching-experiments">
-              <wired-button elevation="2">Watch Videos</wired-button>
-            </a>
+            <div className="button-container">
+              <a href="/watching-experiments">
+                <wired-button elevation="2">Watch Videos</wired-button>
+              </a>
+            </div>
           </div>
         </div>
         <div className="row">
           <div className="activity-card blue">
             <h1>Celebrity Projects</h1>
             <h4 className="faded">Try to build other people's projects.</h4>
-            <a href="/">
-              <wired-button elevation="2">Bill Nye and more</wired-button>
-            </a>
+            <div className="button-container">
+              <a href="/">
+                <wired-button elevation="2">Bill Nye and more</wired-button>
+              </a>
+            </div>
           </div>
 
           <div className="activity-card green">
             <h1>Play Date</h1>
             <h4 className="faded">Build projects with others.</h4>
-            <a href="/playdate">
-              <wired-button elevation="2">Start a play date</wired-button>
-            </a>
+            <div className="button-container">
+              <a href="/playdate">
+                <wired-button elevation="2">Start a play date</wired-button>
+              </a>
+            </div>
           </div>
         </div>
       </div>
